caef19cb7a2cff001fa1e1cfb2a9de132ce324aa
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-05 20:51:27 -0300

    Adding exercise 1

---
+++ exercises/block_4/4.3/exercice_1.js
@@ -0,0 +1,13 @@
+let n = 4;
+let asterisco = '*';
+let linha = '';
+
+
+for (index = 0; index < n; index += 1) {
+  linha += asterisco;
+}
+
+for (index = 0; index < n; index += 1) {
+console.log(linha);
+}
+
