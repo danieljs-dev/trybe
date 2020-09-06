7bb5ababcb06d174d5ac04c3b5e1673c3afe5f52
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-05 22:22:53 -0300

    Adding exercise 1

---
+++ exercises/block_7/7.2/exercice_1.js
@@ -0,0 +1,11 @@
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const turnMorningLesson2 = () => {
+  lesson2.turnMorning = 'Manhã'
+}
+
+console.log(lesson2);
