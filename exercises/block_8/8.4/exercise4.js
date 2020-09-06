1d2a7f6b871c7afbc2673be25a36dd300a002156
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-05 22:50:49 -0300

    Adding exercise 4

---
+++ exercises/block_8/8.4/exercise4.js
@@ -0,0 +1,40 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+// escreva filterPeople abaixo
+
+const { bornIn } = people;
+
+const filteredPeople = people => people.filter((age) => age.bornIn >= 1901 && age.bornIn <= 2000);
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
